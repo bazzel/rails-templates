e32d9407616172d081b88dfd28b01034f1073446
Patrick Baselier <patrick@kabisa.nl>
2014-01-31 18:57:02 +0100

    Start working on Rails template for ember

---
+++ ember.rb
@@ -0,0 +1,7 @@
+# rails new APP_PATH --template=https://raw.github.com/bazzel/rails-templates/master/ember.rb
+# bundle exec rake rails:template LOCATION=https://raw.github.com/bazzel/rails-templates/master/ember.rb
+puts
+puts '==  Rails template for setting up Ember.js ================================'
+puts '-- see: http://emberjs.com/ and https://github.com/emberjs/ember-rails for more'
+puts
+
