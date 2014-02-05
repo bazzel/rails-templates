44b8b4f31913eeb92a97f4f0a7de99a9885c7882
Patrick Baselier <patrick@kabisa.nl>
2014-02-05 08:49:23 +0100

    Setup Ember part of authentication

--- ember-simple-auth.rb
+++ ember-simple-auth.rb
@@ -42,3 +42,47 @@ remove_dir(tmp_folder)
 #
 # End Build and use ember-simple-auth
 
+inject_into_file 'app/assets/javascripts/application.js.coffee', after: "#= require ember\n" do
+  "#= require #{script_name}\n"
+end
+
+# Enable Ember.SimpleAuth in a custom initializer first:
+file 'app/assets/javascripts/initializers/simple_auth.js.coffee', <<-CODE
+Ember.Application.initializer
+  name: 'authentication',
+  initialize: (container, application) ->
+    Ember.SimpleAuth.setup(application)
+CODE
+
+prepend_file 'app/assets/javascripts/app.js.coffee', <<-CODE
+#= require_tree ./initializers
+CODE
+
+# then implement the respective mixin in the application route:
+file 'app/assets/javascripts/routes/application_route.js.coffee', <<-CODE
+App.ApplicationRoute = Ember.Route.extend Ember.SimpleAuth.ApplicationRouteMixin
+CODE
+
+prepend_file 'app/assets/javascripts/templates/application.handlebars', <<-CODE
+
+{{#if session.isAuthenticated}}
+  <a {{ action 'invalidateSession' }}>Logout</a>
+{{else}}
+  {{#link-to 'login'}}Login{{/link-to}}
+{{/if}}
+
+CODE
+
+inject_into_file 'app/assets/javascripts/router.js.coffee', after: /^App\.Router\.map.*\n/ do
+  "  @route 'login'\n"
+end
+
+file 'app/assets/javascripts/templates/login.handlebars', <<-CODE
+<form {{action 'authenticate' on='submit'}}>
+  <label for="identification">Login</label>
+  {{input id='identification' placeholder='Enter Login' value=identification}}
+  <label for="password">Password</label>
+  {{input id='password' placeholder='Enter Password' type='password' value=password}}
+  <button type="submit">Login</button>
+</form>
+CODE
